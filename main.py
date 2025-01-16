f4b662df5a37c129e0044993df200f114cd8b051
RaffiRahman <rafirahmanit21@gmail.com>
2025-01-16 22:05:42 +0600

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from redis import get_redis_connection
+from redis_om import get_redis_connection, HashModel
+
 
 app = FastAPI()
 
@@ -10,3 +11,45 @@ app.add_middleware(
     allow_methods = ['*'],
     allow_headers = ['*']
 )
+
+redis = get_redis_connection(
+    host = 'redis-10298.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port = 10298,
+    password = 't4rxe5UIKDth1wGapKe3nCytZZ2JxLWh',
+    decode_responses = True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+    class Meta:
+        database = redis
+        model_config = {"arbitrary_types_allowed": True}
+
+@app.post('/product')
+def create(product: Product):
+    return product.save()
+
+@app.get('/product/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.get('/products')
+def get_all():
+    # return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.delete('/product/{pk}')
+def delete(pk :str):
+    return Product.delete(pk)
+    
